34f6b6d8e4fa624a95c4cafb5dff91b77f4c103d
PsYcHo96 <JArscott@programmer.net>
2017-05-29 12:30:02 +0100

    Basic implementation of CBaseUICanvas
    - Elements slotted into the canvas are scaled when the canvas is resized
    
    - Currently no implementation of height-only scaling
    
    - Example of usage in UIFrameworkTest
    
    TODO:
    	- Implement height-only scaling
    	- Make canvas movable

--- McEngine/src/App/UIFrameworkTest/UIFrameworkTest.h
+++ McEngine/src/App/UIFrameworkTest/UIFrameworkTest.h
@@ -11,6 +11,8 @@
 #include "App.h"
 
 class CBaseUIButton;
+class CBaseUICanvas;
+class CBaseUITextbox;
 
 class UIFrameworkTest: public App {
 public:
@@ -20,9 +22,21 @@ public:
 	virtual void draw(Graphics *g);
 	virtual void update();
 
+	void resizeCanvas();
+
 public:
 	// UI
 	CBaseUIButton *m_anchorTestButton;
+
+	CBaseUICanvas *m_canvasTest;
+	CBaseUITextbox *m_canvasTestTL;
+	CBaseUITextbox *m_canvasTestTR;
+	CBaseUITextbox *m_canvasTestBL;
+	CBaseUITextbox *m_canvasTestBR;
+	CBaseUIButton *m_canvasTestButton;
+	bool m_bResized;
+
+
 };
 
 #endif /* APP_UIFRAMEWORKTEST_UIFRAMEWORKTEST_H_ */
