d248d38457854cb7af53b55b5c1945201fbff814
PsYcHo96 <JArscott@programmer.net>
2017-06-02 14:15:18 +0100

    New Layout Containers
    - CBaseUIContainerBox
    
    - CBaseUIContainerHorizontalBox - Not Working yet

---
+++ McEngine/src/GUI/CBaseUIContainerHorizontalBox.cpp
@@ -0,0 +1,17 @@
+/*
+ * CBaseUIContainerHorizontalBox.cpp
+ *
+ *  Created on: Jun 1, 2017
+ *      Author: Psy
+ */
+
+#include <CBaseUIContainerHorizontalBox.h>
+
+CBaseUIContainerHorizontalBox::CBaseUIContainerHorizontalBox()
+{
+}
+
+CBaseUIContainerHorizontalBox::~CBaseUIContainerHorizontalBox()
+{
+}
+
